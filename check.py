bce7019948a268ef8eae302b2722c2775b859067
DovydasAL <davidlazauskas1@gmail.com>
2019-05-16 07:40:23 -0400

    remove print statements, add comments, add default value for checking delay

--- check.py
+++ check.py
@@ -35,6 +35,7 @@ def printToStdOut(message):
 	sys.stdout.flush()
 
 def checkClasses(classes, delay):
+	#Base case, all classes have been checked
 	if len(classes) == 0:
 		return
 	time.sleep(delay)
@@ -43,27 +44,31 @@ def checkClasses(classes, delay):
 		soup = BeautifulSoup(response.text, features="html.parser")
 		elements = soup.find_all("td", class_="tableHeader".split())
 		enroll = None
+		#If we find an element that contains 'Enroll', the next td contains enrollment number
 		for element in elements:
 			if element.get_text() == 'Enroll':
 				enroll = element.findNext('td').get_text()
 				break
 		if enroll == None:
 			print("Unable to parse data from URL #" + str(i))
+		#If the enrollment is closed (maybe do something?)
 		elif enroll == 'CLOSED':
 			continue
+		#Display notification, print message, remove from array, call function with rest of array
 		else:
 			notification = Notification("Your class has opened up for the URL #" + str(i + 1))
 			notification.displayNotification()
 			printToStdOut("Your class has opened up for the url " + str(classes[i]) + "\n")
 			classes.pop(i)
-			print(classes)
 			checkClasses(classes, delay)
 			return
 	checkClasses(classes, delay)
 
 if __name__ == "__main__":
 	urls = open('links.txt').read().split('\n')
-	sleep_time = int(sys.argv[1])
+	sleep_time = 15
+	if len(sys.argv) == 2:
+		sleep_time = int(sys.argv[1])
 	for url in range(0, len(urls)):
 		if urls[url] == '':
 			urls.pop(url)
